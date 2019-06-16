b3820f46e8bf5d9a68ed8450fbc3a3dd337c76e0
Jacob Strieb <jacobstrieb@gmail.com>
2019-06-16 11:21:49 -0700

    Request and store API key from user

--- src/groupmearchivergui/FXMLDocumentController.java
+++ src/groupmearchivergui/FXMLDocumentController.java
@@ -1,35 +1,122 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * GroupMe Archiver GUI functions
  */
 package groupmearchivergui;
 
+import static groupmearchivergui.GroupMeArchiverGUI.error;
+
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
+import java.util.prefs.Preferences;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 
 /**
+ * The following code is organized based on the desired flow of user
+ * interaction. As such, it is broken down into steps that the user will follow
+ * to use the program.
  *
- * @author jacobstrieb
+ * @author Jacob Strieb
  */
 public class FXMLDocumentController implements Initializable {
+
+    /**
+     *************************************************************************
+     * Global Variables
+     *************************************************************************
+     */
+    private String API_KEY = "";
+    private Preferences preferences = Preferences.userRoot().node(this.getClass().getName());
     
+
+    /**
+     *************************************************************************
+     * Step 1: User enters API key
+     *************************************************************************
+     */
     @FXML
-    private Label label;
-    
+    private TextField apiKeyTextField;
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private CheckBox rememberApiKeyCheckBox;
+    @FXML
+    private ListView groupListView;
+    @FXML
+    private VBox optionsPanel;
+
+    /**
+     * Handle a user entering an API key and pressing the button by listing all
+     * of the user's groups in the list panel on the left
+     *
+     * @param event unused
+     */
+    @FXML
+    private void handleApiKeyButtonAction(ActionEvent event) {
+        API_KEY = apiKeyTextField.getText();
+
+        if (API_KEY == null || API_KEY.equals("")) {
+            error("No API key entered");
+            return;
+        }
+
+        if (rememberApiKeyCheckBox.isSelected()) {
+            // Only proceed if the user acknowledges the risk of storing their key
+            Alert warning = new Alert(Alert.AlertType.WARNING, "", ButtonType.YES, ButtonType.NO);
+            warning.setHeaderText("Proceed with caution");
+            warning.setContentText("API keys should be treated like passwords. "
+                    + "This application does not store your keys securely. Please "
+                    + "only proceed if you understand the risks. Are you sure you "
+                    + "want to save your API key?");
+            Optional<ButtonType> warningResult = warning.showAndWait();
+
+            if (warningResult.get() == ButtonType.YES) {
+                preferences.put("API_KEY", API_KEY);
+            }
+        }
+        
+        // TODO: Get the group list and check that the API key is valid
+        // TODO: Set the group list in the panel on the left
+        
+        optionsPanel.setDisable(false);
     }
+    
+    
+
+    /**
+     *************************************************************************
+     * Menu Items
+     *************************************************************************
+     */
+    
+    /**
+     * Remove a stored API key by replacing it with the empty string
+     * 
+     * @param event unused
+     */
+    @FXML
+    private void handleRemoveApiKeyMenuItem(ActionEvent event) {
+        preferences.put("API_KEY", "");
+    }
+
+    
+
+    /**
+     *************************************************************************
+     * Miscellaneous
+     *************************************************************************
+     */
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        // Load a remembered API key from storage if possible
+        apiKeyTextField.setText(preferences.get("API_KEY", ""));
+    }
+
 }
--- src/groupmearchivergui/GroupMeArchiverGUI.java
+++ src/groupmearchivergui/GroupMeArchiverGUI.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Main file to run the GroupMe archiver GUI
  */
 package groupmearchivergui;
 
@@ -9,23 +7,42 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
 /**
  *
- * @author jacobstrieb
+ * @author Jacob Strieb
  */
 public class GroupMeArchiverGUI extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        // Configure the window
+        stage.setTitle("GroupMe Archiver");
+        setUserAgentStylesheet(STYLESHEET_MODENA);
         
+        // Load the layout
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         Scene scene = new Scene(root);
         
-        stage.setTitle("GroupMe Archiver");
+        // Display the window
         stage.setScene(scene);
         stage.show();
+    }
+
+    /**
+     * Show an error dialog to the user
+     *
+     * @param errorMessage the message that will be displayed in the main part
+     * of the error dialog window
+     */
+    public static void error(String errorMessage) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error!");
+        alert.setHeaderText("Error!");
+        alert.setContentText(errorMessage);
+        alert.showAndWait();
     }
 
     /**
